07e563edabed42aa8afedf64be76105f5e4e46e6
MagicBowen <e.bowen.wang@icloud.com>
2020-08-19 20:36:08 +0800

    remove unused header include

--- include/mcl/array.h
+++ include/mcl/array.h
@@ -1,7 +1,6 @@
 #ifndef HFB29BCA2_4A0D_414A_BF4C_97FFFA140FC9
 #define HFB29BCA2_4A0D_414A_BF4C_97FFFA140FC9
 
-#include "mcl/symbol.h"
 #include "mcl/static_assert.h"
 #include "mcl/log.h"
 #include <stdlib.h>

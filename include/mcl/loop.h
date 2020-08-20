88cca951293616bfb2fa6497727ba776aa78d02c
MagicBowen <e.bowen.wang@icloud.com>
2020-08-20 20:21:54 +0800

    add loop

---
+++ include/mcl/loop.h
@@ -0,0 +1,17 @@
+#ifndef H90B6970A_7AC3_43F4_9432_0647CA0925A0
+#define H90B6970A_7AC3_43F4_9432_0647CA0925A0
+
+#include "mcl/stdc.h"
+#include <stddef.h>
+
+MCL_STDC_BEGIN
+
+#define MCL_FOR_EACH(type, i, begin, end)   for(type i = begin; i< end; ++i)
+
+#define MCL_FOR_EACH_INDEX(i, max) FOR_EACH(size_t, i, 0, max)
+
+#define MCL_FOREVER()  while(true)
+
+MCL_STDC_END
+
+#endif

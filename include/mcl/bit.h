30a3df22134bddc3475d30e7de913db00d3adc28
magicbowen <e.bowen.wang@icloud.com>
2021-11-05 21:42:05 +0800

    rename bit

--- include/mcl/bit.h
+++ include/mcl/bit.h
@@ -3,19 +3,19 @@
 
 #include <stdint.h>
 
-#ifndef BIT_MASK
-#define BIT_MASK(BIT_NUM)       				\
+#ifndef MCL_BIT_MASK
+#define MCL_BIT_MASK(BIT_NUM)       				\
 (((uint64_t)1 << BIT_NUM) - 1)
 #endif
 
-#ifndef GET_BITS_VALUE
-#define GET_BITS_VALUE(target, offset, length)	\
-(((target) >> (offset)) & BIT_MASK(length))
+#ifndef MCL_GET_BITS_VALUE
+#define MCL_GET_BITS_VALUE(target, offset, length)	\
+(((target) >> (offset)) & MCL_BIT_MASK(length))
 #endif
 
-#ifndef IS_BIT_ON
-#define IS_BIT_ON(target, offset) 				\
-(GET_BITS_VALUE(target, offset, 1) > 0)
+#ifndef MCL_IS_BIT_ON
+#define MCL_IS_BIT_ON(target, offset) 				\
+(MCL_GET_BITS_VALUE(target, offset, 1) > 0)
 #endif
 
 #endif

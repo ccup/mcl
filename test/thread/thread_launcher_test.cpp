7f34b8b29cf5d5425d24910dd7e7ad26fbb44d86
magicbowen <e.bowen.wang@icloud.com>
2022-01-13 13:36:47 +0800

    fix test err

--- test/thread/thread_launcher_test.cpp
+++ test/thread/thread_launcher_test.cpp
@@ -5,6 +5,8 @@
 
 namespace {
 	struct ThreadCtxt {
+		size_t increaseMax;
+		size_t decreaseMax;
 		size_t increaseCount;
 		size_t decreaseCount;
 		MclAtom result;
@@ -13,18 +15,20 @@ namespace {
 
 	void increase(void *ctxt) {
 		ThreadCtxt *threadCtxt = (ThreadCtxt*)ctxt;
-		for (size_t i = 0; (i < threadCtxt->increaseCount); i++) {
+		for (size_t i = 0; (i < threadCtxt->increaseMax); i++) {
 			if (MclAtom_IsTrue(&threadCtxt->stop)) return;
 			MclAtom_Add(&threadCtxt->result, 1);
+			threadCtxt->increaseCount++;
 			MclThread_Yield();
 		}
 	}
 
 	void decrease(void *ctxt) {
 		ThreadCtxt *threadCtxt = (ThreadCtxt*)ctxt;
-		for (size_t i = 0; (i < threadCtxt->decreaseCount); i++) {
+		for (size_t i = 0; (i < threadCtxt->decreaseMax); i++) {
 			if (MclAtom_IsTrue(&threadCtxt->stop)) return;
 			MclAtom_Sub(&threadCtxt->result, 1);
+			threadCtxt->decreaseCount++;
 			MclThread_Yield();
 		}
 	}
@@ -37,7 +41,7 @@ namespace {
 
 FIXTURE(ThreadLauncherTest) {
 	TEST("should async launch threads") {
-		ThreadCtxt ctxt = {.increaseCount = 100, .decreaseCount = 99, .result = 0, .stop = 0};
+		ThreadCtxt ctxt = {.increaseMax = 100, .decreaseMax = 99, .result = 0, .stop = 0};
 
 		MclThreadInfo threads[] = {
 			MCL_THREAD_INFO("Increase", increase, NULL, &ctxt),
@@ -47,12 +51,14 @@ FIXTURE(ThreadLauncherTest) {
 		MclThreadLauncher_Launch(threads, MCL_ARRAY_SIZE(threads));
 		MclThreadLauncher_WaitDone(threads, MCL_ARRAY_SIZE(threads));
 
+		ASSERT_EQ(100, ctxt.increaseCount);
+		ASSERT_EQ(99, ctxt.decreaseCount);
 		ASSERT_EQ(1, ctxt.result);
 	}
 
 	TEST("should stop launched threads") {
 		const uint32_t MAX_COUNT = MCL_UINT32_MAX;
-		ThreadCtxt ctxt = {.increaseCount = MAX_COUNT, .decreaseCount = MAX_COUNT, .result = 0, .stop = 0};
+		ThreadCtxt ctxt = {.increaseMax = MAX_COUNT, .decreaseMax = MAX_COUNT, .result = 0, .stop = 0};
 
 		MclThreadInfo threads[] = {
 			MCL_THREAD_INFO("Increase", increase, stop, &ctxt),

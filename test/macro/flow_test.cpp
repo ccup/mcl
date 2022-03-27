588fb2dfccdcfbc83117f009de0ddbcd6317f2a2
magicbowen <e.bowen.wang@icloud.com>
2022-03-27 10:50:20 +0800

    modify flow test

--- test/macro/flow_test.cpp
+++ test/macro/flow_test.cpp
@@ -1,14 +1,14 @@
 #include <cctest/cctest.h>
 #include "mcl/macro/flow.h"
 
-#define TEST_RESULT(decl, init, result)		\
-MCL_MACRO_FLOW_DECLARE(1, decl)				\
-MCL_MACRO_FLOW_BEFORE(2, i = init)			\
-MCL_MACRO_FLOW_AFTER(3, ASSERT_EQ(i, result))
+#define TEST_RESULT(type, var, init, result)\
+MCL_MACRO_FLOW_DECLARE(1, type var)			\
+MCL_MACRO_FLOW_BEFORE(2, var = init)		\
+MCL_MACRO_FLOW_AFTER(3, ASSERT_EQ(var, result))
 
 FIXTURE(MacroCtrlTest) {
 	TEST("should repeat init array items") {
-		TEST_RESULT(int i, 3, 6) {
+		TEST_RESULT(int, i, 3, 6) {
 			i = i * 2;
 		}
 	}
